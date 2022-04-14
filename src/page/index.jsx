e66612db907e4ffe09de8e9ee621aeaac3c12851
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-04-13 21:22:38 -0500

    feat: routing details country

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./Login";
 import Countries from "./Countries";
+import CountryDetail from "./CountryDetails";
 import { Routes, Route } from "react-router-dom";
 
 const AppRouter = () => {
@@ -8,6 +9,7 @@ const AppRouter = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/countries" element={<Countries />} />
+      <Route path="/details" element={<CountryDetail />} />
     </Routes>
   );
 };
